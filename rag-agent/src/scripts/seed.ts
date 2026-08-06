b148d07db6f37772f884ab556cf7e7da5f1a9882
takayuki murata <muratata@scala-com.jp>
2026-08-06 14:09:38 +0900

    Until creating the index

--- rag-agent/src/scripts/seed.ts
+++ rag-agent/src/scripts/seed.ts
@@ -1,5 +1,8 @@
 import { MDocument } from "@mastra/rag";
 import fs from "fs";
+import { mastra } from "../mastra";
+import { embedMany } from "ai";
+import { ModelRouterEmbeddingModel } from "@mastra/core/llm";
 
 const documents = [
   {
@@ -16,6 +19,13 @@ const documents = [
   }
 ];
 
+const vectoreStore = mastra.getVector("libSqlVector");
+
+await vectoreStore.createIndex({
+  indexName: "company_docs",
+  dimension: 3072,
+});
+
 for (const { filePath, sourceName } of documents) {
   const text = fs.readFileSync(filePath, "utf-8");
   const doc = MDocument.fromMarkdown(text);
@@ -29,5 +39,25 @@ for (const { filePath, sourceName } of documents) {
   });
 
   console.log(`${sourceName}; ${chunks.length} チャンクを保存しました`);
+
+  const { embeddings } = await embedMany({
+    model: new ModelRouterEmbeddingModel(
+      "google/gemini-embedding-001",
+    ),
+    values: chunks.map((chunk) => chunk.text),
+  });
+
     
+  await vectoreStore.upsert({
+    indexName: "company_docs",
+    vectors: embeddings,
+    metadata: chunks.map((chunk) => ({
+      text: chunk.text,
+      source: sourceName,
+      section: chunk.metadata?.title || "",
+      createdAt: new Date().toISOString(),
+    })),
+  });
+
+  console.log("全ドキュメントのデータ取り込みが完了しました");
 }
